1551272d0045c567cbb5b35b1a8f5db4d4d0f906
Duishenalieva <liltril783@gmail.com>
2023-12-13 03:06:06 +0600

    removed unused interface

--- src/type.d.ts
+++ src/type.d.ts
@@ -3,12 +3,6 @@ export interface Page {
   text: string;
 }
 
-export interface PageCreate {
-  id: string;
-  title: string;
-  text: string;
-}
-
 export interface NewPage {
   [id: string]: {
     title: string;
